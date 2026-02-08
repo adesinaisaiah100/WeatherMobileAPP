ff4a871e8704be82fd0ae5eb5fb200f1a43b2fe6
adesinaisaiah100 <166018187+adesinaisaiah100@users.noreply.github.com>
2026-02-08 15:08:15 +0100

    refactor: remove unused api route

--- app/api/weatherdata.ts
+++
@@ -1,7 +0,0 @@
-import NextRequest, { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const city = searchParams.get("city") || "New York";
-
-}
